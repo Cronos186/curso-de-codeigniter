864a13760c4a1934710213b3a29c4ee9123c1ad7
Eborio Linárez <contacto@todoprogramacion.com.ve>
2014-11-05 08:43:05 -0430

    Modelo. Obtiene el listado de artículos.

---
+++ 05-aplicacion-dinamica-sencilla-parte-ii/models/articulos_model.php
@@ -0,0 +1,17 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Articulos_model extends CI_Model {
+
+	function lista_articulos() {
+		$this->db->order_by('id_articulo', 'desc');
+		$consulta = $this->db->get('articulos');
+		return $consulta->result();
+
+		// Retornar los datos en forma de arreglo
+		// return $consulta->result_array();
+	}
+
+}
+
+/* End of file articulos_model.php */
+/* Location: ./application/models/articulos_model.php */
