c90723919779c350a5f1b36705f66e180c79337d
Eborio Linárez <contacto@todoprogramacion.com.ve>
2014-11-05 08:42:33 -0430

    Controlador. Obtiene los datos a través del modelo y los envía a la vista.

---
+++ 05-aplicacion-dinamica-sencilla-parte-ii/controllers/home.php
@@ -0,0 +1,26 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+	public function __construct() {
+		parent::__construct();
+		$this->load->model('Articulos_model');
+
+		// Cargar un modelo cambiando el nombre del objeto
+		// $this->load->model('Articulos_model','art');
+	}
+
+	function index() {
+		$datos['articulos'] = $this->Articulos_model->lista_articulos();
+
+		// Utilizacion del modelo a traves del nombre de objeto modificado
+		// $datos['articulos'] = $this->art->lista_articulos();
+
+		$datos['titulo'] = 'Título de mi sitio - Curso de CI';
+		$this->load->view('index',$datos);
+	}
+
+}
+
+/* End of file home.php */
+/* Location: ./application/controllers/home.php */
