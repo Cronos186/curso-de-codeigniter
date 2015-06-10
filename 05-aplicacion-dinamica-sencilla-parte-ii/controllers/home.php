e62109affaa8e2eab23a1d2c64a9f3c3bc21ae3a
Eborio Linárez <contacto@todoprogramacion.com.ve>
2015-06-09 21:33:14 -0430

    Nuevos comentarios

--- 05-aplicacion-dinamica-sencilla-parte-ii/controllers/home.php
+++ 05-aplicacion-dinamica-sencilla-parte-ii/controllers/home.php
@@ -1,25 +1,45 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Home extends CI_Controller {
+/**
+ * Clase Home
+ *
+ * Controlador para manejar todas las solicitudes relacionadas
+ * con los articulos
+ *
+ * @author Eborio Linarez <contacto@todoprogramacion.com.ve>
+ */
+class Home extends CI_Controller
+{
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        
+        // Cargar un modelo de manera basica
+        $this->load->model('Articulos_model');
 
-	public function __construct() {
-		parent::__construct();
-		$this->load->model('Articulos_model');
+        // Cargar un modelo cambiando el nombre del objeto
+        // $this->load->model('Articulos_model','art');
+    }
 
-		// Cargar un modelo cambiando el nombre del objeto
-		// $this->load->model('Articulos_model','art');
-	}
+    /**
+     * Muestra la vista principal con el listado de articulos
+     * 
+     * @return view Vista principal
+     */
+    public function index()
+    {
+        // Se obtiene la lista de articulos
+        $datos['articulos'] = $this->Articulos_model->lista_articulos();
 
-	// Muestra la vista principal con el listado de articulos
-	function index() {
-		$datos['articulos'] = $this->Articulos_model->lista_articulos();
+        // Utilizacion del modelo a traves del nombre de objeto modificado
+        // $datos['articulos'] = $this->art->lista_articulos();
 
-		// Utilizacion del modelo a traves del nombre de objeto modificado
-		// $datos['articulos'] = $this->art->lista_articulos();
-
-		$datos['titulo'] = 'Título de mi sitio - Curso de CI';
-		$this->load->view('index',$datos);
-	}
+        $datos['titulo'] = 'Título de mi sitio - Curso de CI';
+        $this->load->view('index',$datos);
+    }
 
 }
 
